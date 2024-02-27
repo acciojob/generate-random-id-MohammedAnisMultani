6869434546394b4fc3d3a9cf47e06d70c86ceb61
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-02-27 17:49:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,14 @@
 function makeid(l) { 
-var singles = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-var tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-var hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-var thousands = ["", "M", "MM", "MMM", "MMMM"];
-//turn the number to a string, split individual characters and then turn each one to a number.
-var length = l.toString().length;
-var numbers = l.toString().split("").map(Number);
-var roman = '';
-var i = 0;
 
-switch (length) {
-    
-  case 4:
-    roman = thousands[numbers[i]];
-    i++;
-    
-  case 3:
-    roman += hundreds[numbers[i]];
-    i++;
-    
-  case 2:
-    roman += tens[numbers[i]];
-    i++;
-    
-  case 1 :
-    roman += singles[numbers[i]];
-}
-return roman;
-}
+	let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = ''
+	let charctersLength = charcters.length
+	for(let i=0; i<l; i++){
+ result += characters.charAt(Math.floor(Math.random()*charctersLength))
+	}
+	return result    
+	
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+const l = prompt("Enter a number."); 
 alert(makeid(l));
